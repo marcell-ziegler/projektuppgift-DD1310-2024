1499c6fb752f7f7c677a6b91d1e4491836c4fbbb
Marcell Ziegler <marcell.ziegler.22@gmail.com>
2024-10-08 00:49:51 +0200

    Carriage and seat model prototype implementation

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ Lagrar ett tåg, dess avgångstid, ankomsttid, startstation, destination och en 
 
 ### Attribut
 - `train_number: int` - unikt tågnummer
-- `start_station: str` - startstation
+- `start: str` - startstation
 - `destination: str` - destination
 - `departure_time: datetime` - avgångstid
 - `arrival_time: datetime` - ankomsttid
@@ -30,18 +30,14 @@ Lagrar en vagn, dess stolar och alla passagerare.
 ### Attribut
 - `seats: list[tuple[list[Seat], list[Seat]]]` - lista med alla par av stolsrader separerade av en gång. Varje element innehåller en tuple av två listor med stolar, en för varje sida av gången.
 - `seating_configuration: str` - sittkonfiguration (ex. "2+2", "3+2" etc.)
+- `number: int` - vagnnummer
 
+### Metoder
+- `get_seat(number: int) -> Seat` - returnerar en specifik stol baserat på nummer
 
 ## Seat
 Innehåller information om en stol, dess position och om den är upptagen.
 
 ### Attribut
 - `number: int` - stolsnummer i vagnen
-- `passenger: Passenger` - passagerare som sitter i stolen
-
-
-## Passenger
-Lagrar information om en passagerare.
-
-### Attribut
-- `seat_number: int | None` - stolsnummer som den sitter på. `None` om inte placerad än.
+- `passenger_name: str` - passagerare som sitter i stolen
---
+++ src/biljettbokning/model.py
@@ -0,0 +1,110 @@
+"""Data model for train booking system"""
+
+import itertools
+import re
+import math
+
+
+class Seat:
+    def __init__(self, number: int, passenger_name: str = ""):
+        self.number = number
+        self.passenger_name = passenger_name
+
+    def __repr__(self):
+        return str(self.number)
+
+
+class Carriage:
+    """Holds all seats of a carriage in the specified configuration"""
+
+    def __init__(self, seating_configuration: str, num_rows: int, carriage_num: int):
+
+        self.number = carriage_num
+        self.seating_configuration = seating_configuration
+        self.num_rows = num_rows
+
+        # Create seants
+
+        self.left_seats, self.right_seats = (
+            int(val) for val in seating_configuration.split("+")
+        )
+
+        # Populate the seats list with seat objects in ascending order
+        self.seats: list[tuple[list[Seat], list[Seat]]] = []
+        seat_num = 0
+        for _ in range(num_rows):
+            left = []
+            for _ in range(self.left_seats):
+                seat_num += 1
+                left.append(Seat(seat_num))
+
+            right = []
+            for _ in range(self.right_seats):
+                seat_num += 1
+                right.append(Seat(seat_num))
+            self.seats.append((left, right))
+
+    @property
+    def seating_configuration(self) -> str:
+        """Seating configuration in the format 'x+y' where 0 <= x,y <= 9 for x,y: int"""
+        return self._seating_configuration
+
+    @seating_configuration.setter
+    def seating_configuration(self, value):
+        # Validate type
+        if not isinstance(value, str):
+            raise TypeError("Seating configuration must be a string")
+
+        # Validate format to the property definition
+        if re.match(r"^\d\+\d$", value) is None:
+            raise ValueError(
+                "Invalid seating configuration. Correct examples: '1+2', '3+2', '2+2' etc."
+            )
+
+        self._seating_configuration = value
+
+    def get_seat_num(self, seat_num: int) -> Seat:
+        """Return the seat object for the given seat number in the carriage"""
+        # Total row width
+        row_width = self.left_seats + self.right_seats
+
+        # Get row by rounding up to the nearest integer row based on the index
+        row = math.ceil(seat_num / row_width)
+
+        # Get the index of the seat in the row
+        # row_width * (row - 1) is the number of seats in the previous rows to be removed
+        row_index = seat_num - row_width * (row - 1)
+
+        # Check is seat is on left side
+        if row_index < self.left_seats:
+            return self.seats[row - 1][0][row_index - 1]
+
+        # Seat is on the right side, index subtracts left seats to fit within
+        # the right side tuple
+        return self.seats[row - 1][1][row_index - 1 - self.left_seats]
+
+    def get_seat_name(self, passenger_name: str) -> Seat:
+        """Return the seat object for the given passenger name in the carriage"""
+        seat_filter = filter(
+            lambda s: s.passenger_name == passenger_name,
+            itertools.chain(*itertools.chain(*self.seats)),  # Flatten the list of seats
+        )
+
+        matches = list(seat_filter)
+
+        if len(matches) < 1:
+            raise ValueError(f"No seat found for passenger {passenger_name}")
+
+        if len(matches) > 1:
+            raise ValueError(f"Multiple seats found for passenger {passenger_name}")
+
+        return matches[0]
+
+    def __str__(self):
+        return f"Carriage: {self.seating_configuration} with {self.num_rows} rows"
+
+
+if __name__ == "__main__":
+    c = Carriage("1+3", 10, 1)
+    c.get_seat_num(6).passenger_name = "John Doe"
+    print(c.get_seat_name("John Doe"))
